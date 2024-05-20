c6a7d126d7c8f76cc6869d42d26ad2b8a8e0aa39
bakebrlk <210103194@stu.sdu.edu.kz>
2024-05-21 01:53:31 +0500

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by bakebrlk on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
